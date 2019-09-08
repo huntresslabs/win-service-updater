e8acf02e3988a14048b756459e43a344f65921f9
jdferrell3 <jdferrell3@gmail.com>
2019-09-08 01:22:27 -0400

    download function

---
+++ updater/net.go
@@ -0,0 +1,38 @@
+package updater
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+// DownloadFile will download a url to a local file.
+func DownloadFile(url string, localpath string) error {
+	// Create the file
+	out, err := os.Create(localpath)
+	if nil != err {
+		return err
+	}
+	defer out.Close()
+
+	// Get the data
+	resp, err := http.Get(url)
+	if nil != err {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		err = fmt.Errorf("Error downloading file, HTTP status = %d", resp.StatusCode)
+		return err
+	}
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	if nil != err {
+		return err
+	}
+
+	return nil
+}
