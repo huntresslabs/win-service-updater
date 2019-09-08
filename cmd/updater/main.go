46e1be2b7451fb4742cf2847cd5ca8f636f011ea
jdferrell3 <jdferrell3@gmail.com>
2019-09-08 01:26:21 -0400

    cmd module uses upater module

--- cmd/updater/main.go
+++ cmd/updater/main.go
@@ -2,56 +2,12 @@ package main
 
 import (
 	"fmt"
-
-	"log"
 	"os"
 
-	flags "github.com/jessevdk/go-flags"
+	"github.com/huntresslabs/win-service-updater/updater"
 )
 
-// Args we support
-// "/quickcheck"
-// "/justcheck"
-// "/noerr",
-// "-urlargs="
-// "/outputinfo="
-// "/fromservice"
-// "-logfile="
-
-// https://wyday.com/wybuild/help/wyupdate-commandline.php
-
-type Options struct {
-	// Example of verbosity with level
-	Quickcheck []bool `long:"quickcheck" description:"foo"`
-
-	// Example of verbosity with level
-	Justcheck []bool `long:"justcheck" description:"foo"`
-
-	// Example of verbosity with level
-	Noerr []bool `long:"noerr" description:"foo"`
-
-	// Example of verbosity with level
-	Fromservice []bool `long:"fromservice" description:"foo"`
-
-	// Example of map with multiple default values
-	Urlflags string `long:"urlflags" description:"foo"`
-}
-
-var options Options
-
-var parser = flags.NewParser(&options, flags.Default)
-
 func main() {
-	fmt.Println("FLAGS")
-	_, err := parser.Parse()
-	if err != nil {
-		flagsErr, ok := err.(*flags.Error)
-		if ok && flagsErr.Type == flags.ErrHelp {
-			os.Exit(0)
-		} else {
-			log.Fatal(flagsErr)
-			os.Exit(1)
-		}
-	}
-	fmt.Printf("%+v", options)
+	args := updater.ParseArgs(os.Args)
+	fmt.Printf("%+v", args)
 }
