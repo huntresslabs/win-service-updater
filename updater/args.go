0857b183fe696a01e7594905fc512b327f1e3691
jdferrell3 <jdferrell3@gmail.com>
2019-09-08 01:21:51 -0400

    custom cli parser to match wyupdate args

---
+++ updater/args.go
@@ -0,0 +1,58 @@
+package updater
+
+import (
+	"fmt"
+	"strings"
+)
+
+// Args we support
+// "/quickcheck"
+// "/justcheck"
+// "/noerr",
+// "-urlargs="
+// "/outputinfo="
+// "/fromservice"
+// "-logfile="
+
+// https://wyday.com/wybuild/help/wyupdate-commandline.php
+
+type Args struct {
+	Quickcheck  bool
+	Justcheck   bool
+	Noerr       bool
+	Fromservice bool
+	Urlargs     string
+	Outputinfo  string
+	Logfile     string
+}
+
+func ParseArgs(argsSlice []string) Args {
+	var args Args
+	for _, arg := range argsSlice {
+		larg := strings.ToLower(arg)
+		fmt.Println(larg)
+
+		switch {
+		case larg == "/quickcheck":
+			args.Quickcheck = true
+		case larg == "/justcheck":
+			args.Justcheck = true
+		case larg == "/noerr":
+			args.Noerr = true
+		case larg == "/fromservice":
+			args.Fromservice = true
+		case larg == "/justcheck":
+			args.Justcheck = true
+		case strings.HasPrefix(larg, "-urlargs="):
+			fields := strings.Split(larg, "=")
+			args.Urlargs = fields[1]
+		case strings.HasPrefix(larg, "-logfile="):
+			fields := strings.Split(larg, "=")
+			args.Logfile = fields[1]
+		case strings.HasPrefix(larg, "/outputinfo="):
+			fields := strings.Split(larg, "=")
+			args.Outputinfo = fields[1]
+		}
+	}
+	return args
+}
